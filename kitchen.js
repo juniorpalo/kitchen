04af1069a60d4503c926c9fa073abcfb8ef0cbf2
Juan Palomino <juniorpalo16@gmail.com>
2018-03-29 19:35:13 -0600

    more kitchen stuff

--- kitchen.js
+++ kitchen.js
@@ -8,37 +8,37 @@ class Kitchen {
 }
 
 class Fridge {
-    constructor(haspower, iscool){
+    constructor(haspower, iscool) {
         this.haspower = haspower
         this.iscool = iscool
     }
-    switchpower(){
+    switchpower() {
         this.haspower = !this.haspower
         return this.haspower
     }
-    keepcool(){
+    keepcool() {
         this.iscool = !this.iscool
         return this.iscool
     }
 }
 
 class Microwave {
-    constructor(haspower, cook){
+    constructor(haspower, cook) {
         this.haspower = haspower
         this.cook = cook
     }
-    switchpower(){
+    switchpower() {
         this.haspower = !this.haspower
         return this.haspower
     }
-    quickcook(){
+    quickcook() {
         this.cook = !this.cook
         return this.cook
     }
 }
 
-const fridge = new Fridge(true)
-const microwave = new Microwave(true)
+const fridge = new Fridge(true, false)
+const microwave = new Microwave(false, false)
 
 console.log(fridge.switchpower(), fridge.keepcool())
 console.log(microwave.switchpower(), microwave.quickcook())
