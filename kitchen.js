1cb0092dd177f40d6501090db61166538029c6b4
Juan Palomino <juniorpalo16@gmail.com>
2018-04-02 19:34:43 -0600

    more js stuff

--- kitchen.js
+++ kitchen.js
@@ -1,5 +1,15 @@
 'use strict'
 
+class Appliance {
+    constructor(haspower) {
+        this.haspower = haspower
+    }
+    switchpower() {
+        this.haspower = !this.haspower
+        return this.haspower
+    }
+}
+
 class Kitchen {
     constructor(fridge, microwave) {
         this.fridge = fridge
@@ -7,30 +17,24 @@ class Kitchen {
     }
 }
 
-class Fridge {
+class Fridge extends Appliance {
     constructor(haspower, iscool) {
-        this.haspower = haspower
+        super(haspower)
         this.iscool = iscool
     }
-    switchpower() {
-        this.haspower = !this.haspower
-        return this.haspower
-    }
+
     keepcool() {
         this.iscool = !this.iscool
         return this.iscool
     }
 }
 
-class Microwave {
+class Microwave extends Appliance {
     constructor(haspower, cook) {
-        this.haspower = haspower
+        super(haspower)
         this.cook = cook
     }
-    switchpower() {
-        this.haspower = !this.haspower
-        return this.haspower
-    }
+
     quickcook() {
         this.cook = !this.cook
         return this.cook
@@ -42,5 +46,5 @@ const microwave = new Microwave(false, false)
 const kitchen = new Kitchen(fridge, microwave)
 const kitchen2 = new Kitchen(new Fridge(false), new Microwave(true))
 
-console.log('kitchen2','The fridge is', kitchen2.fridge.switchpower(), kitchen2.fridge.keepcool(), kitchen2.microwave.switchpower(), kitchen2.microwave.quickcook())
+console.log('kitchen2', 'The fridge is', kitchen2.fridge.switchpower(), kitchen2.fridge.keepcool(), kitchen2.microwave.switchpower(), kitchen2.microwave.quickcook())
 console.log(kitchen.fridge.switchpower(), kitchen.fridge.keepcool(), kitchen.microwave.switchpower(), kitchen.microwave.quickcook())
